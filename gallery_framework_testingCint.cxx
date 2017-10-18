637b9a1b8608a04ea27319d737571053b08026ce
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2017-10-18 09:29:14 -0500

    bringing up-to-date

--- gallery_framework_testingCint.cxx
+++ gallery_framework_testingCint.cxx
@@ -38,6 +38,8 @@
 namespace std {} using namespace std;
 
 // Header files passed as explicit arguments
+#include "event.h"
+#include "flash.h"
 #include "tester.h"
 
 // Header files passed via #pragma extra_include
@@ -57,7 +59,7 @@ namespace ROOT {
       ::galleryfmwk::tester *ptr = 0;
       static ::TVirtualIsAProxy* isa_proxy = new ::TIsAProxy(typeid(::galleryfmwk::tester));
       static ::ROOT::TGenericClassInfo 
-         instance("galleryfmwk::tester", "tester.h", 44,
+         instance("galleryfmwk::tester", "tester.h", 46,
                   typeid(::galleryfmwk::tester), ::ROOT::Internal::DefineBehavior(ptr, ptr),
                   &galleryfmwkcLcLtester_Dictionary, isa_proxy, 4,
                   sizeof(::galleryfmwk::tester) );
@@ -88,6 +90,96 @@ namespace ROOT {
 } // end of namespace ROOT
 
 namespace ROOT {
+   static TClass *galleryfmwkcLcLevent_Dictionary();
+   static void galleryfmwkcLcLevent_TClassManip(TClass*);
+   static void *new_galleryfmwkcLcLevent(void *p = 0);
+   static void *newArray_galleryfmwkcLcLevent(Long_t size, void *p);
+   static void delete_galleryfmwkcLcLevent(void *p);
+   static void deleteArray_galleryfmwkcLcLevent(void *p);
+   static void destruct_galleryfmwkcLcLevent(void *p);
+
+   // Function generating the singleton type initializer
+   static TGenericClassInfo *GenerateInitInstanceLocal(const ::galleryfmwk::event*)
+   {
+      ::galleryfmwk::event *ptr = 0;
+      static ::TVirtualIsAProxy* isa_proxy = new ::TIsAProxy(typeid(::galleryfmwk::event));
+      static ::ROOT::TGenericClassInfo 
+         instance("galleryfmwk::event", "event.h", 40,
+                  typeid(::galleryfmwk::event), ::ROOT::Internal::DefineBehavior(ptr, ptr),
+                  &galleryfmwkcLcLevent_Dictionary, isa_proxy, 4,
+                  sizeof(::galleryfmwk::event) );
+      instance.SetNew(&new_galleryfmwkcLcLevent);
+      instance.SetNewArray(&newArray_galleryfmwkcLcLevent);
+      instance.SetDelete(&delete_galleryfmwkcLcLevent);
+      instance.SetDeleteArray(&deleteArray_galleryfmwkcLcLevent);
+      instance.SetDestructor(&destruct_galleryfmwkcLcLevent);
+      return &instance;
+   }
+   TGenericClassInfo *GenerateInitInstance(const ::galleryfmwk::event*)
+   {
+      return GenerateInitInstanceLocal((::galleryfmwk::event*)0);
+   }
+   // Static variable to force the class initialization
+   static ::ROOT::TGenericClassInfo *_R__UNIQUE_(Init) = GenerateInitInstanceLocal((const ::galleryfmwk::event*)0x0); R__UseDummy(_R__UNIQUE_(Init));
+
+   // Dictionary for non-ClassDef classes
+   static TClass *galleryfmwkcLcLevent_Dictionary() {
+      TClass* theClass =::ROOT::GenerateInitInstanceLocal((const ::galleryfmwk::event*)0x0)->GetClass();
+      galleryfmwkcLcLevent_TClassManip(theClass);
+   return theClass;
+   }
+
+   static void galleryfmwkcLcLevent_TClassManip(TClass* ){
+   }
+
+} // end of namespace ROOT
+
+namespace ROOT {
+   static TClass *galleryfmwkcLcLflash_Dictionary();
+   static void galleryfmwkcLcLflash_TClassManip(TClass*);
+   static void *new_galleryfmwkcLcLflash(void *p = 0);
+   static void *newArray_galleryfmwkcLcLflash(Long_t size, void *p);
+   static void delete_galleryfmwkcLcLflash(void *p);
+   static void deleteArray_galleryfmwkcLcLflash(void *p);
+   static void destruct_galleryfmwkcLcLflash(void *p);
+
+   // Function generating the singleton type initializer
+   static TGenericClassInfo *GenerateInitInstanceLocal(const ::galleryfmwk::flash*)
+   {
+      ::galleryfmwk::flash *ptr = 0;
+      static ::TVirtualIsAProxy* isa_proxy = new ::TIsAProxy(typeid(::galleryfmwk::flash));
+      static ::ROOT::TGenericClassInfo 
+         instance("galleryfmwk::flash", "flash.h", 39,
+                  typeid(::galleryfmwk::flash), ::ROOT::Internal::DefineBehavior(ptr, ptr),
+                  &galleryfmwkcLcLflash_Dictionary, isa_proxy, 4,
+                  sizeof(::galleryfmwk::flash) );
+      instance.SetNew(&new_galleryfmwkcLcLflash);
+      instance.SetNewArray(&newArray_galleryfmwkcLcLflash);
+      instance.SetDelete(&delete_galleryfmwkcLcLflash);
+      instance.SetDeleteArray(&deleteArray_galleryfmwkcLcLflash);
+      instance.SetDestructor(&destruct_galleryfmwkcLcLflash);
+      return &instance;
+   }
+   TGenericClassInfo *GenerateInitInstance(const ::galleryfmwk::flash*)
+   {
+      return GenerateInitInstanceLocal((::galleryfmwk::flash*)0);
+   }
+   // Static variable to force the class initialization
+   static ::ROOT::TGenericClassInfo *_R__UNIQUE_(Init) = GenerateInitInstanceLocal((const ::galleryfmwk::flash*)0x0); R__UseDummy(_R__UNIQUE_(Init));
+
+   // Dictionary for non-ClassDef classes
+   static TClass *galleryfmwkcLcLflash_Dictionary() {
+      TClass* theClass =::ROOT::GenerateInitInstanceLocal((const ::galleryfmwk::flash*)0x0)->GetClass();
+      galleryfmwkcLcLflash_TClassManip(theClass);
+   return theClass;
+   }
+
+   static void galleryfmwkcLcLflash_TClassManip(TClass* ){
+   }
+
+} // end of namespace ROOT
+
+namespace ROOT {
    // Wrappers around operator new
    static void *new_galleryfmwkcLcLtester(void *p) {
       return  p ? ::new((::ROOT::Internal::TOperatorNewHelper*)p) ::galleryfmwk::tester : new ::galleryfmwk::tester;
@@ -108,9 +200,53 @@ namespace ROOT {
    }
 } // end of namespace ROOT for class ::galleryfmwk::tester
 
+namespace ROOT {
+   // Wrappers around operator new
+   static void *new_galleryfmwkcLcLevent(void *p) {
+      return  p ? ::new((::ROOT::Internal::TOperatorNewHelper*)p) ::galleryfmwk::event : new ::galleryfmwk::event;
+   }
+   static void *newArray_galleryfmwkcLcLevent(Long_t nElements, void *p) {
+      return p ? ::new((::ROOT::Internal::TOperatorNewHelper*)p) ::galleryfmwk::event[nElements] : new ::galleryfmwk::event[nElements];
+   }
+   // Wrapper around operator delete
+   static void delete_galleryfmwkcLcLevent(void *p) {
+      delete ((::galleryfmwk::event*)p);
+   }
+   static void deleteArray_galleryfmwkcLcLevent(void *p) {
+      delete [] ((::galleryfmwk::event*)p);
+   }
+   static void destruct_galleryfmwkcLcLevent(void *p) {
+      typedef ::galleryfmwk::event current_t;
+      ((current_t*)p)->~current_t();
+   }
+} // end of namespace ROOT for class ::galleryfmwk::event
+
+namespace ROOT {
+   // Wrappers around operator new
+   static void *new_galleryfmwkcLcLflash(void *p) {
+      return  p ? ::new((::ROOT::Internal::TOperatorNewHelper*)p) ::galleryfmwk::flash : new ::galleryfmwk::flash;
+   }
+   static void *newArray_galleryfmwkcLcLflash(Long_t nElements, void *p) {
+      return p ? ::new((::ROOT::Internal::TOperatorNewHelper*)p) ::galleryfmwk::flash[nElements] : new ::galleryfmwk::flash[nElements];
+   }
+   // Wrapper around operator delete
+   static void delete_galleryfmwkcLcLflash(void *p) {
+      delete ((::galleryfmwk::flash*)p);
+   }
+   static void deleteArray_galleryfmwkcLcLflash(void *p) {
+      delete [] ((::galleryfmwk::flash*)p);
+   }
+   static void destruct_galleryfmwkcLcLflash(void *p) {
+      typedef ::galleryfmwk::flash current_t;
+      ((current_t*)p)->~current_t();
+   }
+} // end of namespace ROOT for class ::galleryfmwk::flash
+
 namespace {
   void TriggerDictionaryInitialization_libgallery_framework_testing_Impl() {
     static const char* headers[] = {
+"event.h",
+"flash.h",
 "tester.h",
 0
     };
@@ -136,6 +272,8 @@ namespace {
 #pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
 extern int __Cling_Autoloading_Map;
 namespace galleryfmwk{class __attribute__((annotate("$clingAutoload$tester.h")))  tester;}
+namespace galleryfmwk{class __attribute__((annotate("$clingAutoload$event.h")))  event;}
+namespace galleryfmwk{class __attribute__((annotate("$clingAutoload$flash.h")))  flash;}
 )DICTFWDDCLS";
     static const char* payloadCode = R"DICTPAYLOAD(
 #line 1 "libgallery_framework_testing dictionary payload"
@@ -145,11 +283,15 @@ namespace galleryfmwk{class __attribute__((annotate("$clingAutoload$tester.h")))
 #endif
 
 #define _BACKWARD_BACKWARD_WARNING_H
+#include "event.h"
+#include "flash.h"
 #include "tester.h"
 
 #undef  _BACKWARD_BACKWARD_WARNING_H
 )DICTPAYLOAD";
     static const char* classesHeaders[]={
+"galleryfmwk::event", payloadCode, "@",
+"galleryfmwk::flash", payloadCode, "@",
 "galleryfmwk::tester", payloadCode, "@",
 nullptr};
 
